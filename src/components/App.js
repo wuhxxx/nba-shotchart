92bbfa400fb2d128c6ac2643fa770769e4b3fb3a
wuhxxx <wuhxxx@gmail.com>
2018-11-17 14:46:17 -0800

    create TopBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-// import { TopBar } from './TopBar';
+import { TopBar } from "./TopBar";
 // import { Main } from './Main';
 
 class App extends Component {
     render() {
-        return <div className="App">{/* <TopBar/>
-       <Main/> */}</div>;
+        return (
+            <div className="App">
+                <TopBar />
+                {/* <Main/> */}
+            </div>
+        );
     }
 }
 
