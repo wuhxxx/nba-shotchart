18d8c2a3d16aefa5b8764b1c3d4003dd0acdfc2e
wuhxxx <wuhxxx@gmail.com>
2018-11-17 15:36:41 -0800

    Profile component and initial css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { ShotChart } from "./ShotChart.js";
+import { Profile } from "./Profile.js";
 
 export class Main extends Component {
     render() {
         return (
-            <div>
+            <div className="main">
+                <Profile />
                 <ShotChart playerId={2544} />
             </div>
         );
---
+++ src/components/Profile.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class Profile extends Component {
+    render() {
+        return <div className="profile">a</div>;
+    }
+}
