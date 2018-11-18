198ec4338fc4758a26c6df3aa06fcc1f2557e3e7
wuhxxx <wuhxxx@gmail.com>
2018-11-17 16:12:11 -0800

    player profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import { ShotChart } from "./ShotChart.js";
 import { Profile } from "./Profile.js";
+import nba from "nba";
 
 export class Main extends Component {
+    state = {
+        playerId: 201939,
+        playerInfo: {}
+    };
+
+    componentDidMount() {
+        console.log(this.state.playerId);
+        nba.stats
+            .playerInfo({ PlayerID: this.state.playerId })
+            .then(info => {
+                const playerInfo = Object.assign(
+                    info.commonPlayerInfo[0],
+                    info.playerHeadlineStats[0]
+                );
+                this.setState({ playerInfo });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     render() {
         return (
             <div className="main">
-                <Profile />
-                <ShotChart playerId={2544} />
+                {console.log(this.state)}
+                <Profile
+                    playerId={this.state.playerId}
+                    playerInfo={this.state.playerInfo}
+                />
+                <ShotChart playerId={this.state.playerId} />
             </div>
         );
     }
